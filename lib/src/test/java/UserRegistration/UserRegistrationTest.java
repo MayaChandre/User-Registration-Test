ff8b295a0ee6d8468123f110e0bd219e5e5ed49b
mayavathi <mayachandre27@gmail.com>
2021-07-13 06:38:06 -0400

    Valid First Name

---
+++ lib/src/test/java/UserRegistration/UserRegistrationTest.java
@@ -0,0 +1,31 @@
+package UserRegistration;
+
+import org.junit.Assert;
+import org.junit.Test;
+public class UserRegistrationTest {
+
+UserRegistration userRegistration = new UserRegistration();
+
+    @Test
+     public void givenFirstName_WhenFirstLetterUpperCase_ShouldReturnTrue() {
+	 boolean result = userRegistration.validateFirstName("Mayaa");
+	 Assert.assertTrue(result);
+	}
+     @Test
+     public void givenFirstName_WhenLessThanThreeChars_ShouldReturnFalse() {
+	 boolean result = userRegistration.validateFirstName("Ma");
+	Assert.assertFalse(result);
+    }
+
+     @Test
+ 	public void givenFirstName_WhenHasNumber_ShouldReturnFalse() {
+ 		boolean result = userRegistration.validateFirstName("Maya7");
+ 		Assert.assertFalse(result);
+ 	}
+
+    @Test
+    public void givenFirstName_WhenHasSpecialChar_ShouldReturnFalse() {
+	boolean result = userRegistration.validateFirstName("M@ya");
+	Assert.assertFalse(result);
+}
+}
