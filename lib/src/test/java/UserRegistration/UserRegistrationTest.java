aca8693998d57cd7ae81829fa66796e1f22b4ade
mayavathi <mayachandre27@gmail.com>
2021-07-13 06:49:12 -0400

    Valid Last Name

--- lib/src/test/java/UserRegistration/UserRegistrationTest.java
+++ lib/src/test/java/UserRegistration/UserRegistrationTest.java
@@ -28,4 +28,28 @@ UserRegistration userRegistration = new UserRegistration();
 	boolean result = userRegistration.validateFirstName("M@ya");
 	Assert.assertFalse(result);
 }
+    @Test
+	public void givenLastName_WhenFirstLetterUpperCase_ShouldReturnTrue(){
+	    boolean result = userRegistration.validateLastName("Chandre");
+	    Assert.assertTrue(result);
+	}
+	
+	@Test 
+	public void givenLastName_WhenLessThanThreeChar_shouldReturnFalse() {
+		boolean result = userRegistration.validateLastName("Ch");
+		Assert.assertFalse(result);
+	}
+	
+	@Test
+	  public void givenLastName_WhenHasNumber_ShouldReturnFalse() {
+	      boolean result = userRegistration.validateFirstName("Chandre7");
+	      Assert.assertFalse(result);
+	  }
+
+	  @Test
+	  public void givenLastName_WhenHasSpecialChar_ShouldReturnFalse() {
+	      boolean result = userRegistration.validateFirstName("Ch@ndre");
+	      Assert.assertFalse(result);
+	  }
+
 }
